a1661db5e2b91102c5adb2c1b2af9d954ce84996
Sarita Sharma <saritasharma8201@gmail.com>
2024-03-04 15:44:50 +0530

    Final files

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+   
+    <footer className="footer">
+    
+    <ul className="social-icon">
+      <li className="social-icon__item"><a className="social-icon__link" href="#">
+          <ion-icon name="logo-facebook"></ion-icon>
+        </a></li>
+      <li className="social-icon__item"><a className="social-icon__link" href="#">
+          <ion-icon name="logo-twitter"></ion-icon>
+        </a></li>
+      <li className="social-icon__item"><a className="social-icon__link" href="#">
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </a></li>
+      <li className="social-icon__item"><a className="social-icon__link" href="#">
+          <ion-icon name="logo-instagram"></ion-icon>
+        </a></li>
+    </ul>
+    <ul className="menu">
+      <li className="menu__item"><a className="menu__link" href="#">Home</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">About</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">Services</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">Team</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">Contact</a></li>
+
+    </ul>
+    <p>&copy;2023 Sarita Sharma | All Rights Reserved</p>
+  </footer>
+//   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+//   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+  )
+}
+
+export default Footer;
