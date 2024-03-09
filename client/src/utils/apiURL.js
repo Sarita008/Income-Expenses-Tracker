70d1e0ef972fb4b7665b20eebb4d8c00b72602da
Sarita Sharma <saritasharma8201@gmail.com>
2024-03-10 00:53:03 +0530

    apiURL updated

--- client/src/utils/apiURL.js
+++ client/src/utils/apiURL.js
@@ -1,7 +1,9 @@
 export const API_URL_USER =
-  "http://localhost:9000/api/v1/users";
+  " https://income-expenses-tracker1.onrender.com/api/v1/users";
 export const API_URL_ACC =
-  "http://localhost:9000/api/v1/accounts";
+  " https://income-expenses-tracker1.onrender.com/api/v1/accounts";
 export const API_URL_TRANSACTION =
-  "http://localhost:9000/api/v1/transactions";
+  " https://income-expenses-tracker1.onrender.com/api/v1/transactions";
+
+
 
